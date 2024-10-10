1a8fd108e47dd60cf6a38b3fe71c8394978d822c
yuvrajgrover6 <yuvrajgrover6@gmail.com>
2024-10-10 08:48:58 -0400

    created triangle test class and test for equilateral

--- TriangleTest.cs
+++ TriangleTest.cs
@@ -7,7 +7,7 @@ namespace TriangleTests
     {
         // 1. Equilateral Triangle Test (All sides are equal)
         [Test]
-        public void DetermineTriangleType_WhenSidesAreEqual_ShouldReturnEquilateral()
+        public void Testing_Triangle_As_Equilateral()
         {
             string result = TriangleSolver.DetermineTriangleType(3, 3, 3);
             Assert.AreEqual("Equilateral", result);
